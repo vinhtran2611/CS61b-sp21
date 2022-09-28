8957d040c2d13d340f6d0ca89b28b7b89421caee
vinhtran <tranquocvinh2611@gmail.com>
2022-09-28 21:42:40 +0700

    lab3 - Timing the getLast method of SLList

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -21,8 +21,43 @@ public class TimeSLList {
         timeGetLast();
     }
 
+    public static AList<Integer> creatAList(int begin, int nums){
+        int lastElem = begin;
+        AList<Integer> Ns = new AList<Integer>();
+        for(int i = 0; i < nums; i++){
+            Ns.addLast(lastElem);
+            lastElem *= 2;
+        }
+        return Ns;
+    }
+
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = creatAList(1000, 8);
+        AList<Double> times = new AList<Double>();
+        AList<Integer> opCounts = new AList<Integer>();
+
+        for(int i = 0; i < Ns.size(); i++){
+            //Create an SLList.
+            SLList<Integer> tempList = new SLList<Integer>();
+
+            //Add N items to the SLList.
+            for(int n = 0; n < Ns.get(i); n++){
+                tempList.addLast(n);
+            }
+
+            //Start the timer.
+            Stopwatch sw = new Stopwatch();
+            //Perform M getLast operations on the SLList.
+            for(int k = 0; k < 10000; k++){
+                int val = tempList.getLast();
+            }
+            times.addLast(sw.elapsedTime());
+            opCounts.addLast(10000);
+        }
+
+        printTimingTable(Ns, times, opCounts);
+
     }
 
 }
